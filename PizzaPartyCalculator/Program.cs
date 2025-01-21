01dc2558d6d9942c38b9446f9f5496a3f0d7dc0e
Dr. Mary Lebens <164679441+marylebensanokatech@users.noreply.github.com>
2025-01-21 19:14:17 +0000

    Initial Commit of Pizza Party Calculator

---
+++ PizzaPartyCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿//Pizza Party Calculator
+//Author: Dr. Mary Lebens
+//Date: 1/21/2025
+
+//Declare variables
+int numberOfGuests;
+int slicesPerGuest = 3; //Assumes each guest wants 3 pieces
+int slicesPerPizza = 8; //Standard pizza has 8 slices 
+
+//Prompt the user to enter the number of guests
+Console.Write("Enter the number of guests: ");
+
+//Call the parse function to turn the value the user entered into an int
+numberOfGuests = int.Parse(Console.ReadLine());
+
+//Calculate the total number of slices needed:
+int totalSlicesNeeded = numberOfGuests * slicesPerGuest;
+
+//Calculate the number of pizzas required
+int pizzasNeeded = (int)Math.Ceiling((double)totalSlicesNeeded / slicesPerPizza);
+
+//Output the number of pizzas needed
+Console.WriteLine($"For {numberOfGuests} guests, you'll need {pizzasNeeded} pizzas.");
